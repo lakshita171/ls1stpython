c262ce1c540922f0aabda6dce7bac0e65aa796ad
serionmon <iamp3890@gmail.com>
2026-04-08 11:37:57 +0530

    first

---
+++ python1.py/if assignment.py
@@ -0,0 +1,26 @@
+#assignment-1
+
+a=int(input("enter a number"))
+if(a>0):
+    print("it is a positive number")
+elif(a<0):
+    print("it is a negative number")
+else:
+    print("it is zero")
+
+#assignment-2
+
+a=int(input("enter a number"))
+if(a%2==0):
+    print("it is a even number")
+else:
+    print("it is a odd number")
+
+#assignment-3
+
+
+age=int(input("enter your age"))
+if(age>=18):
+    print("adult")
+else:
+    print("minor")
