ef8edc30b347d24dfe75bc5d70091864c037fae7
serionmon <iamp3890@gmail.com>
2026-04-13 12:55:31 +0530

    loop assign

--- python1.py/loop assign.py
+++ python1.py/loop assign.py
@@ -1,14 +1,27 @@
-for i in range(0,100,1):
-   if(i%2==0 and i%3==0):
-       print(i)
+#for i in range(0,100,1):
+  #  if(i%2==0 and i%3==0):
+      #  print(i)
 
-a=input("enter any name")
-reverse= ""
-for x in a:
-    reverse = x + reverse
-print (reverse)
+#a=input("enter any name")
+#reverse= ""
+#for x in a:
+   # reverse = x + reverse
+#print (reverse)
 
-b=reversed(a)
-print("reversed name is=", b)
-for i in b :
-    print(i, end =" ")
+#b=reversed(a)
+#print("reversed name is=", b)
+#for i in b :
+ #   print(i, end =" ")
+#a= [0,1,2,3,4,5]
+#for x in a:
+ #   if(x==3):
+   #     break
+  #  print(x)
+for i in range(1,101):
+    if i<2:
+        print(i)
+        continue
+    for j in range(2,i):
+        if i%j==0:
+            print(i)
+            break
