64a89f31615cfb4c976d3bab9ab3b48ec33b3fa8
serionmon <iamp3890@gmail.com>
2026-04-17 14:33:02 +0530

    list assign

---
+++ python1.py/list oper.py
@@ -0,0 +1,29 @@
+# program1
+# a=int(input("enter first numbers"))
+# b=int(input("enter second number"))
+# c=int(input("enter third number"))
+# print("value of a=",a)
+# print("value of b=",b)
+# print("value of c=",c)
+# d=[a,b,c]
+# print(d)
+# program2
+# a=[1,2,3,4,5]
+# b=a.insert(1,20)
+# print(a)
+# #program3
+# a=[1,2,3,4]
+# b=[5,6,7,8]
+# a.extend(b)
+# print(a)
+#program 4
+# a=[1,2,3,4,5]
+# b=a.insert(0,20)
+# print(a)
+#program5
+# a=[1,2,3,4]
+# b=[5,6,7,8]
+# a+b
+# print(a+b)
+#program6
+a=[1,2,3,2]
