17ab5ec43e3ff9a59d07799130fbd2f157f6f0db
serionmon <iamp3890@gmail.com>
2026-04-20 10:43:22 +0530

    data type

---
+++ python1.py/data types.py
@@ -0,0 +1,15 @@
+str1="hello world"
+print(str1)
+
+list1=[5,3.3,'new delhi','20c',45]
+print(list1)
+
+tuple1=(10,20,"apple",3.3)
+print(tuple1)
+
+dict1={'fruit':'apple','climate':'cold','price':120}
+print(dict1)
+print(dict1['price'])
+
+set1={1,2,3,4}
+print(set1)
